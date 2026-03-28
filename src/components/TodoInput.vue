2547b3ce6419c429a917efe670367b1e66b5971d
Syeong <pyqw_@naver.com>
2026-03-28 19:14:43 +0900

    Feat: todo 입력 기능 구현
    - v-model로 입력값 양방향 바인딩
    - 엔터 및 버튼 클릭 이벤트 등록
    - 입력값 콘솔 출력 및 초기화 처리

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="todo__input">
     <input
+      v-model="inputMsg"
       type="text"
       class="todo__input-text"
       placeholder="할 일을 입력하세요."
+      @keydown.enter="addTodo"
     />
-    <button class="todo__input-btn">등록</button>
+    <button class="todo__input-btn" @click="addTodo">등록</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputMsg: '',
+    };
+  },
+  methods: {
+    addTodo() {
+      console.log(this.inputMsg);
+      this.inputMsg = '';
+    },
+  },
+};
+</script>
